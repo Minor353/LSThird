daeabd7e8b0ffffa6b91c7b6e7643b8c8f7a0083
Hennadii Nikitenko <vankert353@gmail.com>
2019-07-08 23:24:59 +0300

    Адаптация под мобайл. Верстка и скрипты для мобильного меню.

--- src/main.js
+++ src/main.js
@@ -7,3 +7,4 @@ import "./scripts/skills";
 import "./scripts/header";
 import "./scripts/navScroll";
 import "./scripts/parallax";
+import "./scripts/navOpen";
--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,3 +1,4 @@
+/**Main parallax */
 const parent = document.querySelector("body");
 const item1 = document.querySelector(".parallax-7");
 const item2 = document.querySelector(".parallax-6");
@@ -6,6 +7,12 @@ const item4 = document.querySelector(".parallax-4");
 const item5 = document.querySelector(".parallax-3");
 const item6 = document.querySelector(".parallax-2");
 const item7 = document.querySelector(".parallax-1");
+
+/**reviews parallax*/
+const reviewsParallax1 = document.querySelector(".paralax-reviews_layer-1");
+const reviewsParallax2 = document.querySelector(".paralax-reviews_layer-2");
+const reviewsParallax3 = document.querySelector(".paralax-reviews_layer-3");
+const reviewsParallax4 = document.querySelector(".paralax-reviews_layer-4");
 
 
 var center = {
@@ -20,26 +27,44 @@ parent.onmousemove = function (event) {
     Math.round((center.y - event.pageY) / 85) + 'px, 0);');
 
   item2.setAttribute('style', 'transform: translate3d(' +
-    Math.round((center.x - event.pageX) / 60) + 'px, ' +
-    Math.round((center.y - event.pageY) / 60) + 'px, 0);');
+    Math.round((center.x - event.pageX) / 90) + 'px, ' +
+    Math.round((center.y - event.pageY) / 80) + 'px, 0);');
 
   item3.setAttribute('style', 'transform: translate3d(' +
-    Math.round((center.x - event.pageX) / 50) + 'px, ' +
+    Math.round((center.x - event.pageX) / 30) + 'px, ' +
     Math.round((center.y - event.pageY) / 70) + 'px, 0);');
 
   item4.setAttribute('style', 'transform: translate3d(' +
-    Math.round((center.x - event.pageX) / 10) + 'px, ' +
+    Math.round((center.x - event.pageX) / 20) + 'px, ' +
     Math.round((center.y - event.pageY) / 15) + 'px, 0);');
 
   item5.setAttribute('style', 'transform: translate3d(' +
-    Math.round((center.x - event.pageX) / 40) + 'px, ' +
+    Math.round((center.x - event.pageX) / 30) + 'px, ' +
     Math.round((center.y - event.pageY) / 10) + 'px, 0);');
 
   item6.setAttribute('style', 'transform: translate3d(' +
-    Math.round((center.x - event.pageX) / 25) + 'px, ' +
+    Math.round((center.x - event.pageX) / 65) + 'px, ' +
     Math.round((center.y - event.pageY) / 85) + 'px, 0);');
 
   item7.setAttribute('style', 'transform: translate3d(' +
     Math.round((center.x - event.pageX) / 20) + 'px, ' +
     Math.round((center.y - event.pageY) / 80) + 'px, 0);');
+
+  /********************************/
+  reviewsParallax1.setAttribute('style', 'transform: translate3d(' +
+    Math.round((center.x - event.pageX) / 85) + 'px, ' +
+    Math.round((center.y - event.pageY) / 85) + 'px, 0);');
+
+  reviewsParallax2.setAttribute('style', 'transform: translate3d(' +
+    Math.round((center.x - event.pageX) / 35) + 'px, ' +
+    Math.round((center.y - event.pageY) / 40) + 'px, 0);');
+
+  reviewsParallax3.setAttribute('style', 'transform: translate3d(' +
+    Math.round((center.x - event.pageX) / 45) + 'px, ' +
+    Math.round((center.y - event.pageY) / 45) + 'px, 0);');
+
+  reviewsParallax4.setAttribute('style', 'transform: translate3d(' +
+    Math.round((center.x - event.pageX) / 25) + 'px, ' +
+    Math.round((center.y - event.pageY) / 25) + 'px, 0);');
+
 }
