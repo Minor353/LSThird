f607d1f1ba82756db84869ad3fb3e3063d2dbef7
Hennadii Nikitenko <vankert353@gmail.com>
2019-07-11 12:08:15 +0300

    Организация скилов через vue компоненты

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,39 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+
+const skill = {
+  template: "#skills-row-item",
+  props: {
+    skillName: String,
+    skillPercent: Number
+  }
+}
+
+const skillsRow = {
+  template: "#skills-row",
+  props: {
+    skill: Object
+  },
+  components: {
+    skill
+  }
+}
+
+
+new Vue({
+  el: "#skills-container",
+  template: "#skills-list",
+  components: {
+    skillsRow
+  },
+  data() {
+    return {
+      skills: []
+    }
+  },
+  created() {
+    const data = require('../data/skills.json');
+
+    this.skills = data;
+  }
+})
