2136cec7cd75268fc1eeeeeafc5af0ea6d35afa1
Hennadii Nikitenko <vankert353@gmail.com>
2019-07-15 11:20:00 +0300

    Добавлена анимация скилов

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,8 +1,15 @@
+/**Анимация хедера */
 var lastScrollTop = 0;
 window.onscroll = onScroll;
 const header = document.querySelector('.header');
 
+/**Анимация скилов */
+const circleWrap = document.querySelector('.skill_content');
+const blockCord = circleWrap.getBoundingClientRect().top + window.pageYOffset;
+const circleAnimate = document.querySelectorAll('.circles');
+
 function onScroll(e) {
+  /**Анимация хедера */
   var top = window.pageYOffset;
 
 
@@ -21,4 +28,15 @@ function onScroll(e) {
     header.classList.remove('header--scroll-active');
   }
   lastScrollTop = top;
+
+  /**Анимация скилов*/
+  if (window.pageYOffset > (blockCord - 27)) {
+    for (let i = 0; i < circleAnimate.length; i++) {
+      circleAnimate[i].classList.add("circles-active");
+    };
+  } else if (window.pageYOffset < blockCord - 250) {
+    for (let i = 0; i < circleAnimate.length; i++) {
+      circleAnimate[i].classList.remove("circles-active");
+    };
+  };
 }
