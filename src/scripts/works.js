d7364279a2982a329fa2b4798d18450516351417
Hennadii Nikitenko <vankert353@gmail.com>
2019-07-19 22:49:28 +0300

    Реализован слайдер работ на Vue

---
+++ src/scripts/works.js
@@ -0,0 +1,98 @@
+import Vue from 'vue';
+
+const sliderTags = {
+  template: "#slider-tags",
+  props: {
+    tags: Array
+  }
+}
+
+const sliderBtns = {
+  template: "#slider-btns"
+}
+
+const sliderThumbs = {
+  template: "#slider-thumbs",
+  props: {
+    works: Array,
+    currentWork: Object
+  }
+}
+
+const sliderDisplay = {
+  template: "#slider-display",
+  props: {
+    works: Array,
+    currentWork: Object
+  },
+  computed: {
+    reversedWorks() {
+      return [...this.works].reverse()
+    }
+  },
+  components: {
+    sliderThumbs,
+    sliderBtns
+  }
+}
+
+const sliderInfo = {
+  template: "#slider-info",
+  props: {
+    currentWork: Object
+  },
+  computed: {
+    tagsArray() {
+      return this.currentWork.skills.split(', ');
+    }
+  },
+  components: {
+    sliderTags
+  }
+}
+
+new Vue({
+  el: "#slider-component",
+  template: "#slider-container",
+  components: {
+    sliderDisplay,
+    sliderInfo
+  },
+  data() {
+    return {
+      works: [],
+      currentIndex: 0
+    }
+  },
+  computed: {
+    currentWork() {
+      return this.works[this.currentIndex];
+    }
+  },
+  methods: {
+    makeArrWithRequiredImages(data) {
+      return data.map(item => {
+        const requiredPic = require(`../images/content/${item.photo}`);
+        item.photo = requiredPic;
+
+        return item;
+      });
+    },
+    handleSlide(direction) {
+      switch (direction) {
+        case 'next':
+          this.currentIndex++;
+          break;
+        case 'prev':
+          this.currentIndex--;
+          break;
+      }
+
+    }
+  },
+  created() {
+    const data = require('../data/works.json');
+
+    this.works = this.makeArrWithRequiredImages(data);
+  }
+});
